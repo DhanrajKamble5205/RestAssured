bf1f034f4d7dfc788bc9adb96106d6831ad453be
DhanrajKamble5205 <dhanrajkamble5205@gmail.com>
2025-06-01 12:12:36 +0530

    first

---
+++ RestAssured/src/main/java/com/sdet/RestAssured/App.java
@@ -0,0 +1,13 @@
+package com.sdet.RestAssured;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
